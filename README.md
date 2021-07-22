55c34df55097f3cf12b38a7290bbc3267838c0d0
asjadbut <asjadb18@gmail.com>
2021-07-22 22:00:25 +0500

    Edited readme and server file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# reverse_shell
+# Reverse Shell
 A simple project to create a connection between 2 computers.
---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+import sys
+host = "103.104.214.235"
+port = 9999
+
+#function for creating a socket 
+def create_socket():
+    global s
+    try:
+        s = socket.socket()
+    except:
+        print("Error: " + str(socket.error))
+
+create_socket()
+
+#function for binding the socket and port & host address / listening to connection
+def bind_socket():
+    global s
+    try:
+        print("Binding the port {} and the host {} with the socket".format(str(port),host))
+        s.bind((host,port))
+        s.listen(5)
+    except:
+        print("Error: " + str(socket.error) + "\n" + "Tryng again to bind!")
+        bind_socket()
+
+bind_socket()
+
+#function for establishing a connection with client (socket must be listening )
+def accept_connection ():
+    global s
+    try:
+        print("Accepting the connection!")
+        connection,address  = s.accept()
+        print("Connection established! IP: {} Port: {}".format(str(address[0]),str(address[1])))
+    except:
+        print("Error: " + str(socket.error))
